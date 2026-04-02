35b7399d5659fbdf78a4f6547342b2134ed86f71
devon-romesh <devon.romesh@stu.ocsb.ca>
2026-04-02 13:16:41 -0400

    cide

--- main.ts
+++ main.ts
@@ -2,18 +2,31 @@
  *
  * Created by: Devon
  * Created on: Apr 2026
- * This program can drive a car and avoid a colli !
+ * This program can drive a car and avoid a collision !
 */
 
 
-// setup
-basic.showIcon(IconNames.Happy)
-basic.clearScreen()
-
-// forever loop
+// variables
+let distNum: number = 0
+ 
+ // loop forever
  while (true) {
-    if (input.buttonIsPressed(Button.A) == true )
-   robotbit.StpCarMove(10, 48)
- }
+  distNum = sonar.ping(
+  DigitalPin.P1, // trigger
+  DigitalPin.P2, // echo
+  PingUnit.Centimeters
+  )
+if (input.buttonIsPressed(Button.A) == true){
+ robotbit.StpCarMove(20, 48)
+ basic.showString("I'm moving")
+}
+  //collison avoidence
+  if (distNum > 10){
+    robotbit.StpCarMove(0, 48)
+    pause(500)
+    robotbit.StpCarMove(-10, 48)
+    robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B2)
 
-//
+  }
+ }
+ 
