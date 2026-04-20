26e8bbb63a92dd77e8fb64cb489fc83a2b27e770
devon-romesh <devon.romesh@stu.ocsb.ca>
2026-04-20 18:15:21 -0400

    formatted

--- main.ts
+++ main.ts
@@ -1,55 +1,46 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* * Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: Devon
  * Created on: Apr 2026
- * This program can drive a car and avoid a collision !
-*/
+ * This program can drive a car and avoid a collision!
+ */
 
-// variables
-
+// Variables
 let distance = 0
 
-
-
-// setup
-
+// Setup
 basic.showIcon(IconNames.Happy)
 
-
-
-// button a
-
+// Button A
 input.onButtonPressed(Button.A, function () {
-
     while (true) {
-
-        // read distance sensor
-
+        // Read distance sensor
         distance = sonar.ping(
-            DigitalPin.P8,
-            DigitalPin.P12,
+            DigitalPin.P1,
+            DigitalPin.P2,
             PingUnit.Centimeters
-
         )
+
         basic.showNumber(distance)
         basic.pause(100)
 
         if (distance < 10) {
+            // Stop the car
             robotbit.StpCarMove(0, 0)
             basic.pause(500)
 
-            // reverse 10 cm
+            // Reverse 10 cm
             robotbit.StpCarMove(-10, 48)
             basic.pause(1000)
 
-            // turn 90 degrees
+            // Turn 90 degrees
             robotbit.StpCarTurn(90, 48, 125)
             basic.pause(1000)
-
         } else {
             // Otherwise move forward
             robotbit.StpCarMove(10, 48)
         }
+
         basic.showIcon(IconNames.Heart)
         basic.pause(100)
     }
